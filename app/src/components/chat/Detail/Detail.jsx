91943586471e9fceb0c407989546e4a53cc10bd4
Valentine Efagene <33757347+Valentine-Efagene@users.noreply.github.com>
2023-02-26 16:12:42 +0100

    Feat: Add chat form

--- app/src/components/chat/Detail/Detail.jsx
+++ app/src/components/chat/Detail/Detail.jsx
@@ -1,6 +1,39 @@
-import React from 'react';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import TextArea from '../../inputs/TextArea/TextArea';
 import styles from './Detail.module.css';
 
 export default function Detail() {
-  return <div className={styles.container}>The actual app</div>;
+  const [data, setData] = useState();
+
+  const handleChange = e => {
+    const {
+      target: { name, value },
+    } = e;
+    setData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.chat}></div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <TextArea
+          name="text"
+          onChange={handleChange}
+          placeholder="Talk"
+          className={styles.textArea}
+          value={data?.text}
+          rows="5"
+        />
+        <button className={styles.send}>
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </button>
+      </form>
+    </div>
+  );
 }
---
+++ app/src/components/inputs/TextArea/TextArea.jsx
@@ -0,0 +1,54 @@
+import { func, number, object, oneOf, oneOfType, string } from 'prop-types';
+import React from 'react';
+import styles from './TextArea.module.css';
+
+TextArea.propTypes = {
+  type: string,
+  name: string,
+  id: oneOfType([string, number]),
+  placeholder: string,
+  style: object,
+  className: string,
+  onChange: func,
+  value: string,
+  defaultValue: string,
+  variant: oneOf(['rounded']),
+  rows: number,
+  cols: number,
+};
+
+export default function TextArea({
+  type = 'text',
+  name,
+  id,
+  placeholder,
+  style,
+  className,
+  onChange,
+  value,
+  defaultValue,
+  variant = 'rounded',
+  rows = 10,
+  cols,
+}) {
+  const variantMap = {
+    rounded: styles.rounded,
+    line: styles.line,
+  };
+
+  return (
+    <textarea
+      rows={rows}
+      cols={cols}
+      id={id}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      style={style}
+      className={`${variantMap[variant]} ${styles.container} ${className}`}
+      onChange={onChange}
+      value={value}
+      defaultValue={defaultValue}
+    />
+  );
+}
